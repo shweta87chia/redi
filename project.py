95636383e82d6b5d5564c7dd8991fa719dd9b1d1
Replit user <>
2023-06-05 10:03:46 +0000

    lcm fun defined

--- project.py
+++ project.py
@@ -6,3 +6,11 @@ def extract_number_from_text(text):
       l.append(float(t))
     except ValueError:
       pass
+      return(l)
+
+def lcm(a,b):
+  l=a if a>b else b
+  while l<a*b:
+      if l%a==0 and l%b==0:
+        return l
+        l+=1
