03b6ba2b2e457fe43a4dbbaf21918897e7a991b6
Replit user <>
2023-06-05 10:11:39 +0000

    hcf defined

--- project.py
+++ project.py
@@ -7,10 +7,15 @@ def extract_number_from_text(text):
     except ValueError:
       pass
       return(l)
-
 def lcm(a,b):
   l=a if a>b else b
   while l<a*b:
       if l%a==0 and l%b==0:
         return l
         l+=1
+def hcf(a,b):
+  h=a if a<b else b
+  while h>1:
+    if a%h==0 and b%h==0:
+      return h
+      h-=1
