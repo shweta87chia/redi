a14a346aada201da592f58617f597cfa926695ac
Replit user <>
2023-06-05 10:14:45 +0000

    add subtraction,multiply and divide fun defined

--- project.py
+++ project.py
@@ -19,3 +19,16 @@ def hcf(a,b):
     if a%h==0 and b%h==0:
       return h
       h-=1
+
+def add(a,b):
+  return a+b
+
+def sub(a,b):
+  return a-b
+
+def multiply(a,b):
+  return a*b
+
+def divison(a,b):
+  return a/b
+
